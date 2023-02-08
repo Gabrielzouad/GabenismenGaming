315aa458210c47739198739cca9150474eca14a2
unknown <Z20gab@gmail.com>
2023-02-08 13:27:05 +0100

    added new icon to head

--- components/alert.js
+++ components/alert.js
@@ -1,4 +1,3 @@
-import { Fragment } from "react"
 import { Disclosure, Menu, Transition } from "@headlessui/react"
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
 import { PlusIcon } from "@heroicons/react/20/solid"
--- components/meta.js
+++ components/meta.js
@@ -1,12 +1,11 @@
 import Head from "next/head"
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants"
 
 export default function Meta() {
   return (
     <Head>
       <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
-      <link rel='icon' type='image/png' sizes='32x32' href='/favicon/favicon-32x32.png' />
-      <link rel='icon' type='image/png' sizes='16x16' href='/favicon/favicon-16x16.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/logo.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/logo.png' />
       <link rel='manifest' href='/favicon/site.webmanifest' />
       <link rel='mask-icon' href='/favicon/safari-pinned-tab.svg' color='#000000' />
       <link rel='shortcut icon' href='/favicon/favicon.ico' />
