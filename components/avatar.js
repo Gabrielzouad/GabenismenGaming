1c53df3b05b3ade76487aba9f95ffc3914df99a6
unknown <Z20gab@gmail.com>
2023-02-08 14:12:00 +0100

    new post

--- components/avatar.js
+++ components/avatar.js
@@ -1,17 +1,12 @@
-import ContentfulImage from './contentful-image'
+import ContentfulImage from "./contentful-image"
 
 export default function Avatar({ name, picture }) {
   return (
-    <div className="flex items-center">
-      <div className="relative w-12 h-12 mr-4">
-        <ContentfulImage
-          src={picture.url}
-          layout="fill"
-          className="rounded-full"
-          alt={name}
-        />
+    <div className='flex items-center'>
+      <div className='relative w-12 h-12 mr-4'>
+        <ContentfulImage src={picture.url} layout='fill' className='rounded-full' alt={name} />
       </div>
-      <div className="text-xl font-bold">{name}</div>
+      <div className='text-xl font-bold'>{name}</div>
     </div>
   )
 }
