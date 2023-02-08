c09c6a51f7c7586b3182c4d3d2a9764a4b6b162f
unknown <Z20gab@gmail.com>
2023-02-08 04:45:35 +0100

    added env keys and fixed setup for contentful

--- components/alert.js
+++ components/alert.js
@@ -1,42 +1,111 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { Fragment } from "react"
+import { Disclosure, Menu, Transition } from "@headlessui/react"
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
+import { PlusIcon } from "@heroicons/react/20/solid"
 
-export default function Alert({ preview }) {
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "Players", href: "#", current: false },
+  { name: "Blog", href: "#", current: false },
+  { name: "Matches", href: "#", current: false },
+]
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
+export default function Alert() {
   return (
-    <div
-      className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
-      })}
-    >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This is page is a preview.{' '}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{' '}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
-      </Container>
-    </div>
+    <Disclosure as='nav' className='bg-gray-800'>
+      {({ open }) => (
+        <>
+          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+            <div className='flex h-16 justify-between'>
+              <div className='flex'>
+                <div className='-ml-2 mr-2 flex items-center md:hidden'>
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+                    <span className='sr-only'>Open main menu</span>
+                    {open ? <XMarkIcon className='block h-6 w-6' aria-hidden='true' /> : <Bars3Icon className='block h-6 w-6' aria-hidden='true' />}
+                  </Disclosure.Button>
+                </div>
+                <div className='flex flex-shrink-0 items-center'>
+                  <a href='/'>
+                    <img className='block h-8 w-auto lg:hidden' src='/logo.png' alt='GabenismenTV' />
+                    <img className='hidden h-8 w-auto lg:block' src='/logo.png' alt='GabenismenTV' />
+                  </a>
+                </div>
+                <div className='hidden md:ml-6 md:flex md:items-center md:space-x-4'>
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")}
+                      aria-current={item.current ? "page" : undefined}>
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <div className='flex items-center'>
+                <div className='flex-shrink-0'>
+                  <button
+                    type='button'
+                    className='relative inline-flex items-center rounded-md border border-transparent bg-[#DD4D4A] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800'>
+                    <PlusIcon className='-ml-1 mr-2 h-5 w-5' aria-hidden='true' />
+                    <span>Apply for tryout</span>
+                  </button>
+                </div>
+                <div className='hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center'>
+                  <button
+                    type='button'
+                    className='rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                    <span className='sr-only'>View notifications</span>
+                    <BellIcon className='h-6 w-6' aria-hidden='true' />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className='md:hidden'>
+            <div className='space-y-1 px-2 pt-2 pb-3 sm:px-3'>
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as='a'
+                  href={item.href}
+                  className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "block px-3 py-2 rounded-md text-base font-medium")}
+                  aria-current={item.current ? "page" : undefined}>
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+            <div className='border-t border-gray-700 pt-4 pb-3'>
+              <div className='flex items-center px-5 sm:px-6'>
+                <button
+                  type='button'
+                  className='ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
+                  <span className='sr-only'>View notifications</span>
+                  <BellIcon className='h-6 w-6' aria-hidden='true' />
+                </button>
+              </div>
+              <div className='mt-3 space-y-1 px-2 sm:px-3'>
+                {userNavigation.map((item) => (
+                  <Disclosure.Button key={item.name} as='a' href={item.href} className='block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white'>
+                    {item.name}
+                  </Disclosure.Button>
+                ))}
+              </div>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from "../components/container"
+import MoreStories from "../components/more-stories"
+import HeroPost from "../components/hero-post"
+import Intro from "../components/intro"
+import Layout from "../components/layout"
+import { getAllPostsForHome } from "../lib/api"
+import Head from "next/head"
+import { CMS_NAME } from "../lib/constants"
 
 export default function Index({ preview, allPosts }) {
   const heroPost = allPosts[0]
@@ -16,18 +16,10 @@ export default function Index({ preview, allPosts }) {
         <Head>
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
+
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
+          {heroPost && <HeroPost title={heroPost.title} coverImage={heroPost.coverImage} date={heroPost.date} author={heroPost.author} slug={heroPost.slug} excerpt={heroPost.excerpt} />}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
