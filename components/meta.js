70cd046cdb5e794d2373436374d333bce6d8165e
unknown <Z20gab@gmail.com>
2023-02-10 23:47:27 +0100

    changed meta information

--- components/meta.js
+++ components/meta.js
@@ -3,14 +3,12 @@ import Head from "next/head"
 export default function Meta() {
   return (
     <Head>
-      <link rel='apple-touch-icon' sizes='180x180' href='/favicon/apple-touch-icon.png' />
       <link rel='icon' type='image/png' sizes='32x32' href='/logo.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='/logo.png' />
-      <link rel='manifest' href='/favicon/site.webmanifest' />
-      <link rel='mask-icon' href='/favicon/safari-pinned-tab.svg' color='#000000' />
-      <link rel='shortcut icon' href='/favicon/favicon.ico' />
+      <link rel='manifest' href='/logo.png' />
+      <link rel='mask-icon' href='/logo.png' color='#000000' />
+      <link rel='shortcut icon' href='/logo.png' />
       <meta name='msapplication-TileColor' content='#000000' />
-      <meta name='msapplication-config' content='/favicon/browserconfig.xml' />
       <meta name='theme-color' content='#000' />
       <link rel='alternate' type='application/rss+xml' href='/feed.xml' />
       <meta name='description' content='Esports Gaming News, Reviews, and More | Gabenismen Gaming' />
