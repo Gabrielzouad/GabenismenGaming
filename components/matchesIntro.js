50dd9275691d08c28e6b928911140dd474eb948b
unknown <Z20gab@gmail.com>
2023-02-14 18:33:56 +0100

    added if statements to the matches

--- components/matchesIntro.js
+++ components/matchesIntro.js
@@ -1,7 +1,7 @@
 export default function MatchesIntro() {
   return (
     <>
-      <div className='bg-gray-900 py-24 sm:py-32'>
+      <div className='bg-gray-900 py-10 sm:pt-20'>
         <div className='mx-auto max-w-7xl px-6 text-center lg:px-8'>
           <div className='mx-auto max-w-2xl'>
             <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>Våre Kamper</h2>
--- components/matchinfo.js
+++ components/matchinfo.js
@@ -7,27 +7,54 @@ export default function MatchInfo() {
   useEffect(() => {
     fetchGamerInfo().then((data) => setGames(data))
   }, [])
-
+  const finishedDate = Date(games.finished_at)
   return (
     <>
       <div className='bg-gray-900'>
         <div className='mx-auto max-w-7xl px-6 text-center lg:px-8'>
+          <h2 className='text-2xl font-extrabold text-white sm:text-4xl py-8'>Resultater</h2>
           <ul className='mx-auto grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8'>
-            {games.map((games) => (
-              <div className='container '>
-                <div className='row'>
-                  <div className='card bg-gray-800'>
-                    <div className='card-body'>
-                      <h5 className='card-title'>{games.home_signup.name}</h5>
-                      <h6 className='card-subtitle mb-2 text-muted'>{games.away_signup.name}</h6>
-                      <p className='card-text'>{games.home_score}</p>
-                      <p className='card-text'>{games.away_score}</p>
-                      <p className='card-text'>{games.finished_at}</p>
+            {games.map((games) => {
+              if (games.finished_at !== null && (games.home_signup.name === "Gabenismen Gaming" || games.away_signup.name === "Gabenismen Gaming")) {
+                return (
+                  <div className='container '>
+                    <div className='row'>
+                      <div className='card bg-gray-800'>
+                        <div className='card-body'>
+                          <h5 className='card-title'>{games.home_signup.name}</h5>
+                          <p className='card-text'>{games.home_score}</p>
+                          <h6 className='card-subtitle mb-2 text-muted'>{games.away_signup.name}</h6>
+                          <p className='card-text'>{games.away_score}</p>
+                          <p className='card-text'>{finishedDate}</p>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            ))}
+                )
+              }
+            })}
+          </ul>
+          <h2 className='text-2xl font-extrabold text-white sm:text-4xl py-8'>Kommende kamper</h2>
+          <ul className='mx-auto grid max-w-2xl grid-cols-1 gap-6 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8'>
+            {games.map((games) => {
+              if (games.finished_at === null && (games.home_signup.name === "Gabenismen Gaming" || games.away_signup.name === "Gabenismen Gaming")) {
+                return (
+                  <div className='container '>
+                    <div className='row'>
+                      <div className='card bg-gray-800'>
+                        <div className='card-body'>
+                          <h5 className='card-title'>{games.home_signup.name}</h5>
+                          <h6 className='card-subtitle mb-2 text-muted'>{games.away_signup.name}</h6>
+                          <p className='card-text'>{games.home_score}</p>
+                          <p className='card-text'>{games.away_score}</p>
+                          <p className='card-text'>{finishedDate}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+            })}
           </ul>
         </div>
       </div>
