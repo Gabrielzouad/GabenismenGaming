046c81b2a3e78430aee2b2793576bcd6cf21ca5b
unknown <Z20gab@gmail.com>
2023-02-14 21:49:57 +0100

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "next-themes"
 import "../styles/index.css"
 import Navigation from "../components/navigation"
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute='class'>
       <Navigation />
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
