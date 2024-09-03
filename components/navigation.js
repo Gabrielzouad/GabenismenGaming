af6adff6a37ec2f65ec7a179fa6570f96f0d2eba
Gabrielzouad <z20gab@gmail.com>
2024-09-03 12:00:23 +0200

    changed layout removed unused

--- components/navigation.js
+++ components/navigation.js
@@ -1,38 +1,44 @@
-import { Disclosure } from "@headlessui/react"
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
-import { useTheme } from "next-themes"
+import { Disclosure } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { useTheme } from 'next-themes';
 
 const navigation = [
-  { name: "Hjem", href: "/", current: false },
-  { name: "Spillere", href: "/spillere", current: false },
-  { name: "Blog", href: "/blog", current: false },
-  { name: "Kamper", href: "/matches", current: false },
-]
+  { name: 'Hjem', href: '/', current: false },
+  { name: 'Spillere', href: '/spillere', current: false },
+  { name: 'Blog', href: '/blog', current: false },
+  { name: 'Kamper', href: '/matches', current: false },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ")
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function Navigation() {
-  const { theme, setTheme } = useTheme()
   return (
     <Disclosure as='nav' className='bg-gray-800'>
       {({ open }) => (
         <>
-          <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+          <div className='mx-auto container px-4 sm:px-6 lg:px-8'>
             <div className='flex h-16 justify-between'>
               <div className='flex'>
                 <div className='-ml-2 mr-2 flex items-center md:hidden'>
                   {/* Mobile menu button */}
                   <Disclosure.Button className='inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
                     <span className='sr-only'>Open main menu</span>
-                    {open ? <XMarkIcon className='block h-6 w-6' aria-hidden='true' /> : <Bars3Icon className='block h-6 w-6' aria-hidden='true' />}
+                    {open ? (
+                      <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
+                    ) : (
+                      <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
+                    )}
                   </Disclosure.Button>
                 </div>
                 <div className='flex flex-shrink-0 items-center'>
                   <a href='/'>
-                    <img className='block h-8 w-auto lg:hidden' src='/logo.png' alt='GabenismenTV' />
-                    <img className='hidden h-8 w-auto lg:block' src='/logo.png' alt='GabenismenTV' />
+                    <img
+                      className='block h-8 w-auto '
+                      src='/logo.png'
+                      alt='GabenismenTV'
+                    />
                   </a>
                 </div>
                 <div className='hidden md:ml-6 md:flex md:items-center md:space-x-4'>
@@ -40,8 +46,14 @@ export default function Navigation() {
                     <a
                       key={item.name}
                       href={item.href}
-                      className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "px-3 py-2 rounded-md text-sm font-medium")}
-                      aria-current={item.current ? "page" : undefined}>
+                      className={classNames(
+                        item.current
+                          ? 'bg-gray-900 text-white'
+                          : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        'px-3 py-2 rounded-md text-sm font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
                       {item.name}
                     </a>
                   ))}
@@ -51,7 +63,8 @@ export default function Navigation() {
                 <div className='flex-shrink-0'>
                   <button
                     type='button'
-                    className='relative inline-flex items-center rounded-md border border-transparent bg-[#DD4D4A] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-[#DD4D5A] focus:outline-none focus:ring-2  focus:ring-offset-2 focus:ring-offset-gray-800'>
+                    className='relative inline-flex items-center rounded-md border border-transparent bg-[#DD4D4A] px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-[#DD4D5A] focus:outline-none focus:ring-2  focus:ring-offset-2 focus:ring-offset-gray-800'
+                  >
                     <span>
                       <a href='https://www.twitch.tv/skreppwin'>SE OSS LIVE</a>
                     </span>
@@ -68,8 +81,14 @@ export default function Navigation() {
                   key={item.name}
                   as='a'
                   href={item.href}
-                  className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "block px-3 py-2 rounded-md text-base font-medium")}
-                  aria-current={item.current ? "page" : undefined}>
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
                   {item.name}
                 </Disclosure.Button>
               ))}
@@ -78,5 +97,5 @@ export default function Navigation() {
         </>
       )}
     </Disclosure>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Container from "../components/container"
-import MoreStories from "../components/more-stories"
-import HeroPost from "../components/hero-post"
-import Intro from "../components/intro"
-import Layout from "../components/layout"
-import { getAllPostsForHome } from "../lib/api"
-import Head from "next/head"
+import Container from '../components/container';
+import MoreStories from '../components/more-stories';
+import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPostsForHome } from '../lib/api';
+import Head from 'next/head';
 
 export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout preview={preview}>
@@ -18,17 +18,26 @@ export default function Index({ preview, allPosts }) {
 
         <Container>
           <Intro />
-          {heroPost && <HeroPost title={heroPost.title} coverImage={heroPost.coverImage} date={heroPost.date} author={heroPost.author} slug={heroPost.slug} excerpt={heroPost.excerpt} />}
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
+  const allPosts = (await getAllPostsForHome(preview)) ?? [];
   return {
     props: { preview, allPosts },
-  }
+  };
 }
